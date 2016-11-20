af9510442f7e749daa14eb8f6fa1fc9e1a080a0f
Nikolay <kakushonokcpu@gmail.com>
2016-11-20 22:58:31 +0300

    добавлена возможность расширять протолабиринт в готовый массив. Так же, введен файл с функциями для генерации по алгоритму Прима

--- Maze.lua
+++ Maze.lua
@@ -1,211 +1,63 @@
-local Room = require "Room" --комнаты для лабиринта
-local Conf = require "MazeConfig" --конфигурационный файл лабиринта 
-
---функции работы с протолабиринтом
-local maze = require ("proto")
+local utils    = require ("lua_utils.maputils")
+local neig     = require ("lua_utils.neighborhood")
+local room     = require ("Room")
+local proto    = require ("proto")
 
 -------------Лабиринт--------------
---Лабиринт будет из себя представлять прямоугольную таблицу комнат
+
 local M = {}
 
---Парсер лабиринта из заданого входного файла
-function M:Load (fileName_)
-	local fileIn = io.open (fileName_, "r")
-	
-	--прочитать размерность лабиринта 
-   self.colums, self.rows = fileIn:read ("*number", "*number")
-	
-	--прочитать данные для каждой комнаты лабиринта в соответствии с 
-	--полученным ранее размером
-	for i = 1, self.rows do
-		self[i] = {}
-		
-		for j = 1, self.colums do
-			self[i][j] = Room:New (fileIn:read ("*number", "*number"))
-		end
-	end
-end
-
---сохранение лабиринта в файл
-function M:Save ()
-	--создадим файл для записи
-	--имя для файла
-	local FileName = tostring (os.date ("%H_%M_%S")) .. "_maze_save.txt"
-		
-	local OutputFile = io.open (FileName, "w")
-	
-	--записать размерность лабиринта
-	OutputFile:write (tostring (self.colums) .. " " .. tostring (self.rows) .. "\n")
-	
-	--записать данные для каждой комнаты
-	for i = 1, self.rows do
-		for j = 1, self.colums do
-			local l, t = self[i][j]:GetWall ()
-			OutputFile:write (tostring (l) .. " " .. tostring (t) .. "\n")
-		end
-	end
-end
-
---вывод лабиринта в файл
-function M:Show ()
-	--вариант такой, сразу создать по размерам будущего лабиринта 
-	--поле (двумерный массив)
-	--потом проходиться по структуре лабиринта и для каждой комнаты
-	--писать в пустой массив стены и пустоты
-	
-	--а потом получившейся буфер распечатать в файл
-			
-	--размер представления лабиринта на экране
-	local w, h = self.rows * Conf.roomSize + 1, self.colums * Conf.roomSize + 1
-	
-	--подготовка буфера 
-	local buffer = {}
-	
-	for i = 1, w do
-		buffer[i] = {}
-		
-		for j = 1, h do
-			buffer[i][j] = Conf.empty 
-		end
-	end
-	
-	--внесение данных в буфер в соответствии с тем, 
-	--что же записано в конкретной комнате лабиринта
-	for l = 1, self.rows do
-		for t = 1, self.colums do
-			if debugMode then
-				print ("l: " .. tostring (l) .. " t: " .. tostring (t))
-			end
-			
-			local lWall, tWall = self[l][t]:GetWall ()
-	
-			local l0, t0 = (l - 1) * Conf.roomSize + 1, (t - 1) * Conf.roomSize + 1
-	
-			if tWall == 1 then
-				for itX = l0, l0 + (Conf.roomSize - 1) do
-					buffer[itX][t0] = Conf.wall
-				end
-			end
-	
-			if lWall == 1 then
-				for itY = t0, t0 + (Conf.roomSize - 1) do
-					buffer[l0][itY] = Conf.wall
-				end
-			end			
-		end
-	end	 
-	
-	--по правому и нижнему краю лабиринта необходимо проложить стены
-	for i = 1, w do
-		buffer[i][h] = Conf.wall
-	end
-	
-	for j = 1, h do
-		buffer[w][j] = Conf.wall
-	end
-	
-	--определяем, куда выводить лабиринт
-	if Conf.targetFile then
-		--имя для файла
-		local FileName = tostring (os.date ("%d_%m_%y_%H_%M")) .. "_maze.txt"
-		
-		OutputFile = io.open (FileName, "w")
-	end
-	
-	--выводим полученый результат в файл
-	for i = 1, w do
-		for j = 1, h do
-			--нарисовать
-			if Conf.targetFile then
-				OutputFile:write (buffer[i][j])
-			end
-			
-			if Conf.targetScreen then
-				io.write (buffer[i][j])
-			end
-		end
-		
-		if Conf.targetFile then
-			OutputFile:write ("\n")
-		end
-		
-		if Conf.targetScreen then
-			io.write ("\n")
-		end
-	end --вывод лабиринта в файл	
-	
-end --Maze:Show
-
---есть ли проход между двумя локациями
---передается координата текущел локации и смещение к локации, в которую надо пройти
-function M:IsGo (x_, y_, dx_, dy_)
-		
-	local left, top
-	local rez
-	
-	if dx_ == -1 then
-		left, top =  self[x_][y_]:GetWall ()
-		
-		if left == 0 then 
-			rez = true
-		else 
-			rez = false
-		end
-	elseif dx_ == 1 then
-		left, top =  self[x_ + 1][y_]:GetWall ()
-		
-		if left == 0 then 
-			rez = true
-		else 
-			rez = false
-		end
-	elseif dy_ == -1 then
-		left, top =  self[x_][y_]:GetWall ()
-		
-		if top == 0 then 
-			rez = true
-		else 
-			rez = false
-		end
-	elseif dy_ == 1 then
-		left, top =  self[x_][y_ + 1]:GetWall ()
-		
-		if top == 0 then 
-			rez = true
-		else 
-			rez = false
-		end
-	end
-	
-	return rez
-end
-
---функция сносит стену между двумя локациями в лабиринте
-function M:BreakWall (x_, y_, dx_, dy_)
-	--print ("X: " .. tostring (x_ + dx_) .. " Y: " .. tostring (y_ + dy_))
-
-	local left, top
-		
-	if dx_ == -1 then
-		left, top =  self[x_][y_]:GetWall ()
-		self[x_][y_]:SetWalls (0, top)
-		
-	elseif dx_ == 1 then
-		left, top =  self[x_ + 1][y_]:GetWall ()
-		self[x_ + 1][y_]:SetWalls (0, top)
-		
-	elseif dy_ == -1 then
-		left, top =  self[x_][y_]:GetWall ()
-		self[x_][y_]:SetWalls (left, 0)
-				
-	elseif dy_ == 1 then
-		left, top =  self[x_][y_ + 1]:GetWall ()
-		self[x_][y_ + 1]:SetWalls (left, 0)
-	
-	end	
-end
-
---
+--Функция, расширяющая протолабиринт в готовый для дальнейшего использования
+local function Extend (protoMaze)
+   --забацаем результирующий массив исходя из размеров протолабиринта
+   --размер отдельной комнаты
+   local size = 4
+   
+   local xlen, ylen = #protoMaze[1] * size + 1, #protoMaze * size + 1
+   
+   local buffer = {}
+   utils.SetSize (buffer, xlen, ylen)
+   
+   --print (#buffer)
+   --print (#buffer[1])
+   
+   --нарисуем для каждой конкретной комнаты стены в ней
+   for i, j, val in utils.Iter (protoMaze) do
+      --получим стены для данной ячейки
+      local lWall, tWall = val:GetWall ()
+      
+      --определим из каой точки нужно рисовать стены
+      local l0, t0 = (i - 1) * size + 1, (j - 1) * size + 1 
+      
+      --рисуем стену для верхней стены
+      if tWall == 1 then
+         for tmpY = t0, (t0 + size) do
+            buffer[l0][tmpY] = 1
+         end
+      end
+      
+      --рисуем стену для левой стены
+      if lWall == 1 then
+         --print ("l0 = ",l0)
+         --print ("t0 = ",t0)
+         
+         for tmpX = l0, (l0 + size) do
+            buffer[tmpX][t0] = 1
+         end
+      end
+   end
+   
+   --так же, проводим стены по периметру
+   for l = 1, #buffer do
+      buffer[l][#buffer[1]] = 1
+   end
+   
+   for t = 1, #buffer[1] do
+      buffer[#buffer][t] = 1
+   end
+   
+   return buffer
+end --Extend
 
 --Функция генерации лабиринта заданого размера
 function M.Generate (xlen, ylen)
--- proto.lua
+++ proto.lua
@@ -42,8 +42,8 @@ function M:IsGo (x_, y_, dx_, dy_)
          rez = false
       end
    elseif dx_ == 1 then
-      left, top =  self[x_ + 1][y_]:GetWall ()
-		
+      left, top =  self[x_][y_ + 1]:GetWall ()
+		      
       if left == 0 then 
          rez = true
       else 
@@ -58,7 +58,7 @@ function M:IsGo (x_, y_, dx_, dy_)
 			rez = false
 		end
 	elseif dy_ == 1 then
-		left, top =  self[x_][y_ + 1]:GetWall ()
+		left, top =  self[x_ + 1][y_]:GetWall ()
 		
 		if top == 0 then 
 			rez = true
@@ -68,7 +68,32 @@ function M:IsGo (x_, y_, dx_, dy_)
 	end
 	
 	return rez
-end
+end --IsGo
+
+--функция сносит стену между двумя локациями в лабиринте
+function M:BreakWall (x_, y_, dx_, dy_)
+	--print ("X: " .. tostring (x_ + dx_) .. " Y: " .. tostring (y_ + dy_))
+
+	local left, top
+		
+	if dx_ == -1 then
+		left, top =  self[x_][y_]:GetWall ()
+		self[x_][y_]:SetWalls (0, top)
+		
+	elseif dx_ == 1 then
+		left, top =  self[x_][y_ + 1]:GetWall ()
+		self[x_][y_ + 1]:SetWalls (0, top)
+		
+	elseif dy_ == -1 then
+		left, top =  self[x_][y_]:GetWall ()
+		self[x_][y_]:SetWalls (left, 0)
+				
+	elseif dy_ == 1 then
+		left, top =  self[x_ + 1][y_]:GetWall ()
+		self[x_ + 1][y_]:SetWalls (left, 0)
+	
+	end	
+end --BreakWall
 
 --[[----------------Модульные тесты------------------]]--
 
@@ -92,15 +117,83 @@ local function test_IsGo ()
    --подготовить тестовый лабиринт для проверки
    local maze1 = M:New (4, 4)
    maze1[1][2]:SetWalls (0, 1)
+   maze1[2][3]:SetWalls (0, 1)
+   maze1[2][2]:SetWalls (1, 0)
+   maze1[3][1]:SetWalls (1, 0)
    
    --проверка слева
    --проверить, что между комнатами с проходом есть проход
    assert (maze1:IsGo (1, 2, -1, 0) == true,
-      "Не находится проход между комнатами, когда он есть")
+      "Слева. Не находится проход между комнатами, когда он есть")
    
    --проверить, что между комнатами без прохода нет прохода
    assert (maze1:IsGo (1, 3, -1, 0) == false,
-      "Находится проход между комнатами, где его нет")
+      "Слева. Находится проход между комнатами, где его нет")
+   
+   --проверка справа
+   assert (maze1:IsGo (1, 1 , 1, 0) == true,
+      "Cправа. Не находится проход между комнатами, когда он есть")
+   
+   --print (maze1:IsGo (1, 1 , 1, 0))
+   --print (maze1[1][2]:GetWall ())
+      
+   assert (maze1:IsGo (2, 1, 1, 0) == false,
+      "Справа. Находится проход между комнатами, где его нет")                        
+   
+   --проверка сверху
+   assert (maze1:IsGo (2, 2, 0, -1) == true,
+      "Сверху. Не находится проход между комнатами, когда он есть")
+   
+   assert (maze1:IsGo (2, 1, 0, -1) == false,
+      "Сверху. Находится проход между комнатами, где его нет")
+   
+   --проверка снизу
+   assert (maze1:IsGo (2, 1, 0, 1) == true,
+      "Снизу. Не находится проход между комнатами, когда он есть")
+   
+   assert (maze1:IsGo (2, 2, 0, 1) == false,
+      "Снизу. Находится проход между комнатами, где его нет")
+end
+
+local function test_BreakWall ()
+   --проласываем стены в тестовых лабиринтах, потом проверяем, 
+   --что сломали там, где нужно
+   
+   --ломаем стену направо
+   local maze1 = M:New (3, 3)
+   maze1:BreakWall (2, 2, 1, 0)
+   local rezL1, rezT1 = maze1[2][3]:GetWall ()
+   
+   assert (rezL1 == 0,
+      "Стена направо не сломалась!")
+   maze1 = nil   
+   
+   --ломаем стену налево
+   local maze2 = M:New (3, 3)
+   maze2:BreakWall (2, 2, -1, 0)
+   local rezL2, rezT2 = maze2[2][2]:GetWall ()
+   
+   assert (rezL2 == 0,
+      "Стена налево не сломалась!")
+   maze2 = nil
+      
+   --ломаем стену вверх
+   local maze3 = M:New (3, 3)
+   maze3:BreakWall (2, 2, 0, -1)
+   local rezL3, rezT3 = maze3[2][2]:GetWall ()
+   
+   assert (rezT3 == 0,
+      "Стена вверх не сломалась!")
+   maze3 = nil
+   
+   --ломаем стену вниз
+   local maze4 = M:New (3, 3)
+   maze4:BreakWall (2, 2, 0, 1)
+   local rezL4, rezT4 = maze4[3][2]:GetWall ()
+   
+   assert (rezT4 == 0,
+      "Стена вниз не сломалась!")
+   maze4 = nil
 end
 
 return M
