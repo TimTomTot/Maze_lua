0b44e4e566937a7abd430f89ab47f05651815cc3
TimTomTot <kakushonokcpu@gmail.com>
2017-10-29 21:41:31 +0100

    В модуле world_map добавлен метод парсинга карты из строки. Так же началась переработка модуля world, который теперь будет только контейнером для всех входящих в игру объектов

--- src/world/world_map.lua
+++ src/world/world_map.lua
@@ -9,6 +9,8 @@
 local class = require "30log"
 local Grid = require "jumper.grid"
 
+local CellFactory = require "world.cellfactory"
+
 
 local Map = class("Map")
 
@@ -18,10 +20,47 @@ function Map:init(data)
     end
     
     self.map = Grid(data.map)
+    self.strmap = data.map
     
-    self.stearslist = {}
+    self.stairslist = {}
     self.doorlist = {}
+
+    self.cellfactory = CellFactory:new()
+
+    self:__parseStrMap__(self.strmap)
 end
+
+--[[
+    Переданная карта парсится и на основе того, что в ней занесено,
+    создается карта в коечном массиве Grid
+--]]
+function Map:__parseStrMap__(strmap)
+    local rowIndex, columnIndex = 1, 1
+
+    for row in strmap:gmatch ("[^\n]+") do
+        columnIndex = 1
+
+        for character in row:gmatch (".") do
+            local newcell = self.cellfactory:generateCell(character)
+
+            local node = self.map:getNodeAt(columnIndex, rowIndex)
+
+            node.data = newcell
+
+            if newcell.name == "stairs" then
+                table.insert(self.stairslist, newcell)
+            end
+
+            if newcell.name == "closedoor" or newcell.name == "opendoor" then
+                table.insert(self.doorlist, newcell)
+            end
+
+            columnIndex = columnIndex + 1
+        end
+        rowIndex = rowIndex + 1
+    end
+end
+
 
 function Map:getWidht()
     return self.map:getWidth()
@@ -31,13 +70,12 @@ function Map:getHeight()
     return self.map:getHeight()
 end
 
---[[
-    Разбор каждого символа на переданной карте.
-    Для этой ф-ции задается соответствие символов и конктретных типов ячеек.
-    Все типы которые не поедусмотренны при разборе замещаются на просто тайлы пола
---]]
-function Map:__parseCellInMap__(cell)
+function Map:getStairsList()
+    return self.stairslist
+end
 
+function Map:getDoorList()
+    return self.doorlist
 end
 
 return Map
--- test/world/world_map_test.lua
+++ test/world/world_map_test.lua
@@ -33,3 +33,61 @@ local function test_getMapSizes()
     assert(is_eq(tMap:getWidht(), 8))
     assert(is_eq(tMap:getHeight(), 3))
 end 
+
+local function test_CheckEmptyMap()
+    local tInitdata = {
+        map = [[
+#############
+#...........#
+#...........#
+#...........#
+#############]]
+    }
+
+    local tMap = Map:new(tInitdata)
+
+    local stairs = tMap:getStairsList()
+    local doors = tMap:getDoorList()
+
+    assert(is_eq(#stairs, 0))
+    assert(is_eq(#doors, 0))
+end
+
+local function test_CheckMultiStairsMap()
+    local tInitdata = {
+        map = [[
+#############
+#......>....#
+#...>.......#
+#.......>...#
+#############]]
+    }
+
+    local tMap = Map:new(tInitdata)
+
+    local stairs = tMap:getStairsList()
+    local doors = tMap:getDoorList()
+
+    assert(is_eq(#stairs, 3))
+    assert(is_eq(#doors, 0))
+end
+
+local function test_CheckMultiDoorMap()
+    local tInitdata = {
+        map = [[
+####-########
+#......>..+.#
+#...>.......#
+#....+..>.-.#
+#############]]
+    }
+
+    local tMap = Map:new(tInitdata)
+
+    local stairs = tMap:getStairsList()
+    local doors = tMap:getDoorList()
+
+    assert(is_eq(#stairs, 3))
+    assert(is_eq(#doors, 4))
+end
+
